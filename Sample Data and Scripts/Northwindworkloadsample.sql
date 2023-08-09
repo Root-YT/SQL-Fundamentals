fa15acd37a0043dbd834a795c0187c283eea9e95
Dr. Anandkumar Prakasam <root.anand@gmail.com>
2023-08-09 16:08:29 +0530

    Question Sets, Data and Workload Samples

---
+++ Sample Data and Scripts/Northwindworkloadsample.sql
@@ -0,0 +1,7 @@
+SELECT ProductID, ProductName, UnitPrice FROM Products WHERE (UnitPrice > 12.5) AND (UnitPrice < 14)
+
+DELETE FROM Products WHERE UnitPrice <= 1 
+
+SELECT * FROM PRODUCTS WHERE UnitPrice BETWEEN 12 AND 16 
+
+SELECT * FROM Products ORDER BY UnitPrice ASC 
---
+++ Sample Data and Scripts/workloadsample.sql
@@ -0,0 +1,12 @@
+SELECT ProductID, ProductName, UnitPrice FROM NewProducts WHERE (UnitPrice > 12.5) AND (UnitPrice < 14)
+
+DELETE FROM NewProducts WHERE UnitPrice <= 1 
+
+SELECT * FROM NewPRODUCTS WHERE UnitPrice BETWEEN 12 AND 16 
+
+SELECT * FROM NewProducts ORDER BY UnitPrice ASC 
+
+
+
+
+
